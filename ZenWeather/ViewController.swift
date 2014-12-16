d095848a0e728b58b7e58f771d545d98451f5ff8
cresstoo <cress@duran.im>
2014-12-16 21:22:42 +0800

    Initial Commit

---
+++ ZenWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZenWeather
+//
+//  Created by 哈吉马鹿咚 on 14-12-16.
+//  Copyright (c) 2014年 imduran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
