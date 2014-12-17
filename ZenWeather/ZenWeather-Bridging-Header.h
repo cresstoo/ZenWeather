be4ecc8a089a39ec5d8dadade4cf21cf8aaf6cc4
cresstoo <cress@duran.im>
2014-12-18 00:23:40 +0800

    push to github

---
+++ ZenWeather/ZenWeather-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ZenWeather-Bridging-Header.h
+//  ZenWeather
+//
+//  Created by 哈吉马鹿咚 on 14-12-17.
+//  Copyright (c) 2014年 imduran. All rights reserved.
+//
+
+#ifndef ZenWeather_ZenWeather_Bridging_Header_h
+#define ZenWeather_ZenWeather_Bridging_Header_h
+
+
+#endif
